2878a4abd2b6ff877d3b8529f74cdbaec418f440
neossoftware <mario.hidalgom@yahoo.com.mx>
2025-02-20 20:40:50 -0600

    cambios react

--- react-sample2/src/AppProperties.js
+++ react-sample2/src/AppProperties.js
@@ -9,12 +9,12 @@ import { CORE_CONCEPTS , reactDescriptions} from './data';
 
 function genRandomInt(max)  { return Math.floor(Math.random() * (max+1)); }
 
-function CoreConcept(props) {
+function CoreConcept({image,title, description}) {
   return(
     <li> 
-      <img src={props.image} alt={props.title}/>
-      <h3>{props.title}</h3>
-      <p>{props.description}  </p>
+      <img src={image} alt={title}/>
+      <h3>{title}</h3>
+      <p>{description}  </p>
 
     </li>
   )
